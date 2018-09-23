602817eb5fdce5b9d1686fc262a5f36930a9350d
heguofu <554636911@qq.com>
2018-09-23 19:32:37 +0800

    增加启动类

---
+++ h-user/h-user-client/src/main/java/com/fw/user/boot/BootApplication.java
@@ -0,0 +1,14 @@
+package com.fw.user.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
